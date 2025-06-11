5d10544373855e39561f5836b2476c921697d9f2
sakshi <agnihotrisakshi256@gmail.com>
2025-06-12 02:12:27 +0530

    sakshi_portfolio

---
+++ src/constants.js
@@ -0,0 +1,265 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.png';
+
+// Experience Section Logo's
+import Techpile from './assets/company_logo/Techpile.jpg';
+import agcLogo from './assets/company_logo/agc_logo.png';
+import newtonschoolLogo from './assets/company_logo/newtonschool_logo.png';
+
+// Education Section Logo's
+// import glaLogo from './assets/education_logo/gla_logo.png';
+import College from './assets/education_logo/College.jpg';
+import School from './assets/education_logo/School.jpeg';
+
+// Project Section Logo's
+import insta from './assets/work_logo/github_det.png';
+import recipe from './assets/work_logo/Recipyfinder.jpg';
+import movierecLogo from './assets/work_logo/movie_rec.png';
+import expensetracker from './assets/work_logo/ExpenseTracker.jpg';
+import valentine from './assets/work_logo/Valentineday.jpg';
+import ecommercewebsite from './assets/work_logo/ecommercewebsite.jpg';
+import zepto from './assets/work_logo/10MinZepto.jpg';
+import heartcare from './assets/work_logo/Hearcare.jpg';
+import passwordmanager from './assets/work_logo/Passwordmanager.jpg';
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      // { name: 'SASS', logo: sassLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      // { name: 'Angular', logo: angularLogo },
+      { name: 'Redux', logo: reduxLogo },
+      // { name: 'Next JS', logo: nextjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      // { name: 'GSAP', logo: gsapLogo },
+      // { name: 'Material UI', logo: materialuiLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'DataBase',
+    skills: [
+      // { name: 'Springboot', logo: springbootLogo },
+      // { name: 'Node JS', logo: nodejsLogo },
+      // { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      // { name: 'Firebase', logo: firebaseLogo },
+      // { name: 'PostgreSQL', logo: postgreLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      // { name: 'C++', logo: cppLogo },
+      // { name: 'Java', logo: javaLogo },
+      { name: 'Python', logo: pythonLogo },
+      // { name: 'C-Sharp', logo: csharpLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      // { name: 'TypeScript', logo: typescriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      // { name: 'Compass', logo: mcLogo },
+      { name: 'Vercel', logo: vercelLogo },
+      // { name: 'Netlify', logo: netlifyLogo },
+      // { name: 'Figma', logo: figmaLogo },
+    ],
+  },
+];
+
+  export const experiences = [
+    {
+      id: 0,
+      img: Techpile,
+      role: "Fullstack Developer As a Trainer",
+      company: "Techpile Technology Pvt Ltd.",
+      date: "july 2023 - september 2023",
+      desc: "Developed dynamic and scalable web applications using the MERN stack, handling both frontend and backend development. Collaborated with cross-functional teams to build responsive UI, implement RESTful APIs, and optimize application performance in an agile environment.",
+      skills: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "Django Pyhton",
+        "CSS",
+        
+      ],
+    },
+    
+  ];
+  
+  export const education = [
+    // {
+    //   id: 0,
+    //   img: glaLogo,
+    //   school: "GLA University, Mathura",
+    //   date: "Sept 2022 - July 2024",
+    //   grade: "7.81 CGPA",
+    //   desc: "I have completed my Master's degree (MCA) in Computer Applications from GLA University, Mathura. During my time at GLA, I gained a strong foundation in programming, software development, and computer science principles. I have studied courses such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web Development, and Software Engineering. I actively participated in various workshops and technical events, which enhanced my skills and knowledge. My experience at GLA University has been instrumental in shaping my technical abilities and professional growth.",
+    //   degree: "Master of Computer Applications - MCA",
+    // },
+    {
+      id: 1,
+      img: College,
+      school: "M.G.Institute of Management and Technology",
+      date: "August 2020-june 2024",
+      grade: "8.36 Cgpa",
+      desc: "I completed my Bachelor's degree in Computer Science and Engineering  from M.G.I.M.T. College, Lucknow. Throughout my studies, I was immersed in a variety of subjects that deepened my understanding of computing and technology. From exploring Data Structures and Algorithms to diving into Web Development and Database Management Systems, I gained practical insights into the world of software development. My time at M.G.I.M.T. College allowed me to work on projects that applied theoretical concepts to real-world problems.",
+      degree: "Bachelor of  Computer Science  and Engineering",
+    },
+    {
+      id: 2,
+      img: School,
+      school: "Shri Janki Prasad Inter College Patseni Kachhauna Hardoi",
+      date: "Apr 2018 - March 2020",
+      grade: "70%",
+      desc: "I completed my class 12 education from Shri Janki Prasad Inter College Patseni Kachhauna Hardoi, where I studied Physics, Chemistry, and Mathematics (PCM) with Computer Science.",
+      degree: "U.P.Board(XII) - PCM with Computer Science",
+    },
+    {
+      id: 3,
+      img: School,
+      school: "Shri Janki Prasad Inter College Patseni Kachhauna Hardoi",
+      date: "Apr 2016 - March 2018",
+      grade: "75%",
+      desc: "I completed my class 10 education from Shri Janki Prasad Inter College Patseni Kachhauna Hardoi, where I studied Science with Computer.",
+      degree: "U.P.Board(X), Science with Computer Application",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "Instagram Clone",
+      description:
+         "An Instagram Clone is a full-stack social media application that allows users to log in, create posts, like, comment, and interact with other users in a responsive and engaging interface",
+      image: insta,
+      tags: ["HTML", "TailWind CSS", "JavaScript", "React JS", "API"],
+      github: "https://github.com/codingmastr/GitHub-Profile-Search-App-Using-React-JS",
+      webapp: "https://githubprofiledetective.netlify.app/",
+    },
+    {
+      id: 1,
+      title: "Recipe Finder Website",
+      description:
+        "The Recipe Finder website is a dynamic platform that helps users discover and explore recipes based on ingredients they have. It simplifies meal planning by offering tailored recipe suggestions.",
+      image: recipe,
+      tags: ["React JS", "HTML", "CSS", "JavaScript"],
+      github: "https://github.com/Sakshi123-tech/Recipe-finder-website/tree/master",
+      webapp: "https://recipe-app-five-topaz.vercel.app/",
+    },
+    {
+      id: 2,
+      title: "Movie Recommendation App",
+      description:
+        "A React-based web application that provides movie recommendations based on different criteria, such as genres, user preferences, and popular trends. The intuitive design and smooth experience make it a go-to app for movie enthusiasts.",
+      image: movierecLogo,
+      tags: ["React JS", "API", "HTML", "CSS", "JavaScript"],
+      github: "https://github.com/codingmastr/Movie-Recommendation-App",
+      webapp: "https://movie-recommendation-app-jet.vercel.app/",
+    },
+    {
+      id: 3,
+      title: "Valentine Day web Page",
+      description:
+        "The Valentine's Day Special Website is a personalized web experience designed to celebrate love and togetherness. It allows users to create and share heartfelt messages, relationship milestones, and special moments in a beautifully themed interface.",
+      image: valentine,
+      tags: ["React JS", "CSS", "Html","Javascript"],
+      github: "https://github.com/Sakshi123-tech/velentineform",
+      webapp: "https://sprightly-paletas-d76b7d.netlify.app/",
+    },
+    {
+      id: 4,
+      title: "Expense Tracker",
+      description:
+        "The Expense Tracker is a web application designed to help users manage and track their finances efficiently. It allows users to log expenses, categorize transactions, and visualize spending patterns.",
+      image: expensetracker,
+      tags: ["JavaScript", "Reactjs", "HTML", "CSS"],
+      github: "https://github.com/Sakshi123-tech/expensetracker",
+      webapp: "https://euphonious-madeleine-c71886.netlify.app/",
+    },
+    {
+      id: 5,
+      title: "Ecommerce Website",
+      description:
+        "The E-commerce Website is a dynamic online shopping platform that allows users to browse, purchase, and manage orders seamlessly. It offers a modern shopping experience with secure transactions and a user-friendly interface..",
+      image: ecommercewebsite,
+      tags: ["HTML", "CSS", "JavaScript", "React JS"],
+      github: "https://github.com/Sakshi123-tech/Ecommerceusingreact",
+      webapp: "https://dazzling-shortbread-3d3f4d.netlify.app/",
+    },
+    {
+      id: 6,
+      title: "10 Min Zepto",
+      description:
+        "The 10-Min Zepto is a rapid delivery e-commerce platform that enables users to order groceries and essentials with guaranteed delivery within 10 minutes. This project integrates real-time order processing, inventory management, and location-based logistics.",
+      image: zepto,
+      tags: ["Html","CSS","Javascript", "My Sql", "Python", "Django"],
+      github: "https://github.com/Sakshi123-tech/blinkit",
+      webapp: "https://codingmasterweb.in/",
+    },
+    {
+      id: 7,
+      title: "Heart Care Website",
+      description:
+        "The Heart Care Website is a comprehensive healthcare platform designed to provide users with vital heart health information, appointment scheduling, and emergency assistance. It integrates real-time monitoring, doctor consultations, and personalized health recommendations to promote cardiovascular wellness.",
+      image: heartcare,
+      tags: ["Html","CSS","Javascript", "My Sql", "Python", "Django"],
+      github: "https://github.com/Sakshi123-tech/healthcare",
+      webapp: "https://imagsearch.netlify.app/",
+    },
+    {
+      id: 8,
+      title: "Password Manager",
+      description:
+        "The Password Manager is a secure web application designed to help users store, manage, and retrieve passwords safely. It ensures encrypted storage, allowing users to access their credentials anytime while maintaining security.",
+      image: passwordmanager,
+      tags: ["React JS","HTML", "CSS", "Javascript"],
+      github: "https://github.com/Sakshi123-tech/password-manager",
+      webapp: "https://removeyourbg.netlify.app/",
+    },
+  ];  
